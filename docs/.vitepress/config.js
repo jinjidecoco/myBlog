3d86c7d742d1a944bb2aea674014e61ee1023aa4
coco <coco@192.168.2.11>
2024-04-09 22:14:07 +0800

    feat:修改问题

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,4 +2,8 @@ export default {
   title: "小飒啊", //站点标题
   description: "一个前端工程师的博客", //mate标签description，多用于搜索引擎抓取摘要
   base: "/myBlog/", //部署到github pages的路径
+  footer: {
+    message: "Released under the MITLicense.",
+    copyright: "Copyright © 2024-present xiaosa Blog. All rights reserved.",
+  },
 }
